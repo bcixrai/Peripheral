3292f11b8708490509d33f6877a5723041a48531
odinsen369 <70570229+odinsen369@users.noreply.github.com>
2023-05-31 22:48:07 +0200

    painting works in build

--- Peripheral/Source/Peripheral/Peripheral.Build.cs
+++ Peripheral/Source/Peripheral/Peripheral.Build.cs
@@ -8,7 +8,7 @@ public class Peripheral : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "Niagara" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
